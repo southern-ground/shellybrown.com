d7ae19b351906df226f0eb87ccb3c64d49ece3a5
Ferris Thomas <fsthomas@gmail.com>
2016-12-02 16:54:52 -0500

    Formatting, mostly.

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("api_includes.php");
+include_once "api_includes.php";
 
 define('BRACELET_CATEGORY', '75');
 define('ACTION_DEBUG', 'debug');
@@ -12,21 +12,19 @@ define('ACTION_ADD_TO_CART', 'addtocart');
 
 $action = strtolower($_GET['action'] ?: ACTION_INVENTORY);
 
-$response = array('error' => 1, 'message' => "Error in the lizard brain.");
+$response = array('error'=>1, 'message'=>"Error in the lizard brain.");
 
-if ($action === ACTION_INVENTORY ||
+if($action === ACTION_INVENTORY ||
     $action === ACTION_MSRP ||
     $action === ACTION_SALE_PRICE ||
     $action === ACTION_CATEGORY ||
     $action === ACTION_DEBUG ||
-    $action === ACTION_ADD_TO_CART
-) {
+    $action === ACTION_ADD_TO_CART ) {
 
-    $proxy = new SoapClient(API_URL);
+    $proxy = new SoapClient(API_V2_URL);
     $sessionId = $proxy->login(API_USERNAME, API_PASSWORD);
 
-    function compareByName($a, $b)
-    {
+    function compareByName($a, $b) {
         return strcmp($a->name, $b->name);
     }
 
@@ -34,7 +32,7 @@ if ($action === ACTION_INVENTORY ||
 
         case ACTION_ADD_TO_CART:
 
-            $products = $_GET['skus'] ?: -1;
+            $products = $_GET['skus'] ? : -1;
 
             // create curl resource
             $ch = curl_init();
@@ -57,7 +55,7 @@ if ($action === ACTION_INVENTORY ||
             // close curl resource to free up system resources
             curl_close($ch);
 
-            $response = array('error' => 0, 'message' => "Add to Cart", 'data' => json_decode($output));
+            $response = array('error'=>0, 'message'=>"Add to Cart", 'data'=>json_decode($output));
 
             break;
 
@@ -65,11 +63,11 @@ if ($action === ACTION_INVENTORY ||
 
             // Scrap using v2; use v1.
             $proxy = new SoapClient(API_V1_URL);
-            $session = $proxy->login(API_USERNAME, API_PASSWORD;
+            $session = $proxy->login(API_USERNAME, API_PASSWORD);
 
             $result = $proxy->call($session, 'catalog_product.info', '594');
 
-            $response = array('error' => 0, 'message' => "catalogProductInfo", 'data' => array("productID" => 'SB-B10PG', "result" => $result));
+            $response = array('error'=>0, 'message'=>"catalogProductInfo", 'data'=>array("productID"=>'SB-B10PG', "result"=>$result));
 
             break;
 
@@ -81,7 +79,7 @@ if ($action === ACTION_INVENTORY ||
                 $products = $proxy->catalogCategoryAssignedProducts($sessionId, $categoryID);
                 $product_ids = [];
 
-                foreach ($products as $product) {
+                foreach($products as $product){
                     array_push($product_ids, $product->product_id);
                 }
 
@@ -96,11 +94,11 @@ if ($action === ACTION_INVENTORY ||
 
                 usort($result, 'compareByName');
 
-                $response = array('error' => 0, 'message' => "Category request.", 'data' => array("category" => $categoryID, "items" => $result));
+                $response = array('error'=>0, 'message'=>"Category request.", 'data'=>array("category"=>$categoryID, "items"=>$result));
 
             } catch (SoapFault $fault) {
 
-                $response = array('error' => 998, 'message' => "Invalid category ($categoryID). Code: $fault->faultcode, String: $fault->faultstring");
+                $response = array('error'=>998, 'message'=>"Invalid category ($categoryID). Code: $fault->faultcode, String: $fault->faultstring");
 
             }
 
@@ -111,20 +109,20 @@ if ($action === ACTION_INVENTORY ||
             $productID = $_GET['productID'] ?: -1;
 
             try {
-                $result = $proxy->catalogProductInfo($sessionId, (string)$productID);
+                $result = $proxy->catalogProductInfo($sessionId, (string) $productID);
 
                 $saleItem = false;
-                foreach ($result->categories as $value) {
-                    if ($value === '74') {
+                foreach($result->categories as $value){
+                    if($value === '74'){
                         $saleItem = true;
                         break;
                     }
                 }
 
-                $response = array('error' => 0, 'message' => "Product Price", 'data' => array('product_id' => $result->product_id, 'price' => $result->price, 'on_sale' => $saleItem ? 1 : 0));
+                $response = array('error'=>0, 'message'=>"Product Price", 'data'=>array('product_id'=>$result->product_id, 'price'=>$result->price, 'on_sale'=>$saleItem ? 1 : 0));
 
             } catch (SoapFault $fault) {
-                $response = array('error' => 999, 'message' => "Invalid SKU ($productID). Code: $fault->faultcode, String: $fault->faultstring");
+                $response = array('error'=>999, 'message'=>"Invalid SKU ($productID). Code: $fault->faultcode, String: $fault->faultstring");
             }
 
             break;
@@ -132,47 +130,47 @@ if ($action === ACTION_INVENTORY ||
         case ACTION_INVENTORY: // Fail over to default:
         default:
 
-            // Fail over to default:
-            // Get all items under the category:
-            $products = $proxy->catalogCategoryAssignedProducts($sessionId, BRACELET_CATEGORY);
-            $product_ids = [];
-            foreach ($products as $product) {
-                array_push($product_ids, (string)$product->product_id);
+        // Fail over to default:
+        // Get all items under the category:
+        $products = $proxy->catalogCategoryAssignedProducts($sessionId, BRACELET_CATEGORY);
+        $product_ids = [];
+        foreach ($products as $product) {
+            array_push($product_ids, (string)$product->product_id);
+        }
+
+        // Get all items currently in stock:
+        $in_stock = $proxy->catalogInventoryStockItemList($sessionId, $product_ids);
+
+        $product_ids = [];
+
+        foreach ($in_stock as $product) {
+
+            if ( (bool)$product->is_in_stock ) {
+                array_push($product_ids, $product->product_id);
             }
 
-            // Get all items currently in stock:
-            $in_stock = $proxy->catalogInventoryStockItemList($sessionId, $product_ids);
+        }
 
-            $product_ids = [];
+        // All products w/filter:
+        $complexFilter = array(
+            array(
+                'key' => 'product_id',
+                'value' => array('key' => 'in', 'value' => implode(',', $product_ids))
+            )
+        );
 
-            foreach ($in_stock as $product) {
+        $result = $proxy->catalogProductList($sessionId, array('complex_filter' => $complexFilter)); // Complex filter needed to be wrapped in a separate array, like shown in the docs
 
-                if ((bool)$product->is_in_stock) {
-                    array_push($product_ids, $product->product_id);
-                }
+        usort($result, 'compareByName');
 
-            }
-
-            // All products w/filter:
-            $complexFilter = array(
-                array(
-                    'key' => 'product_id',
-                    'value' => array('key' => 'in', 'value' => implode(',', $product_ids))
-                )
-            );
-
-            $result = $proxy->catalogProductList($sessionId, array('complex_filter' => $complexFilter)); // Complex filter needed to be wrapped in a separate array, like shown in the docs
-
-            usort($result, 'compareByName');
-
-            $response = array('error' => 0, 'message' => "Success.", 'data' => $result);
+        $response = array('error'=>0, 'message'=>"Success.", 'data'=>$result);
     }
 
     // Kill off the session.
     $proxy->endSession($sessionId);
-} else {
+}else{
     // Error.
-    $response = array('error' => 911, 'message' => "Invalid action request ($action)");
+    $response = array('error'=>911, 'message'=>"Invalid action request ($action)");
 }
 
 header('Content-Type: application/json');
